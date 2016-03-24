0d37f3dab28d7e2eb836c07e4dc298f7efe661dd
TabWeng <hlwyfeng@gmail.com>
2016-03-24 21:45:24 +0800

    更新

---
+++ js/myJS/responAdmin.js
@@ -0,0 +1,46 @@
+$(function(){
+
+	myResponsive(
+		"#headTab1",
+		0,630,
+		{
+			"width":"100%"
+		}
+	);
+
+	myResponsive(
+		"#headTab2",
+		0,630,
+		{
+			"width":"100%"
+		}
+	);
+
+	myResponsive(
+		"#headTab3",
+		0,630,
+		{
+			"width":"100%"
+		}
+	);
+
+	myResponsive(
+		"#headTab4",
+		0,630,
+		{
+			"width":"100%"
+		}
+	);
+
+	myResponsive(
+		"#headTab5",
+		0,630,
+		{
+			"width":"100%"
+		}
+	);
+
+
+
+
+});
